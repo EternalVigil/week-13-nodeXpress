07e0105531c8ca0b38da7c79d8d384be9a2ab6c9
Brandon <theeternalvigil@gmail.com>
2017-01-12 12:47:03 -0800

    results.js added comparison logic

---
+++ public/results.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+$.ajax({
+	method: "GET",
+	url: "/api/new",
+	dataType: "json"
+}).done(function (data) {
+	"use strict";
+	var bestMatch = 0;
+	var profileCompareScore = 0;
+	var compatibleScore = 40;
+	var lastIndex = data.length - 1;
+	console.log(data[lastIndex]);
+
+	for (var i = 0; i < data.length - 2; i++) {
+		profileCompareScore = 0;
+		for (var j = 0; j < 10; j++) {
+			profileCompareScore += data[lastIndex].answers[j] - data[i].answers[j];
+		}
+		Math.abs(profileCompareScore);
+
+		if (profileCompareScore < compatibleScore) {
+			bestMatch = i;
+		}
+	}
+});
